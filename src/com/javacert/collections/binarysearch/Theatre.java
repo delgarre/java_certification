f829d8219d36208b15f891759244901d1a8a0ee1
dgarrett <dgarrett@apptricity.com>
2022-03-02 15:13:18 -0600

    Collections Overview

--- src/com/javacert/collections/binarysearch/Theatre.java
+++ src/com/javacert/collections/binarysearch/Theatre.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Theatre {
     private final String theatreName;
-    private List<Seat> seats = new ArrayList<>();
+    public List<Seat> seats = new ArrayList<>();
 
     public Theatre(String theatreName, int numRows, int seatsPerRow){
         this.theatreName = theatreName;
@@ -48,7 +48,7 @@ public class Theatre {
             System.out.println(seat.getSeatNumber());
         }
     }
-    private class Seat implements Comparable<Seat>{
+    public class Seat implements Comparable<Seat>{
         private final String seatNumber;
         private boolean reserved = false;
 
