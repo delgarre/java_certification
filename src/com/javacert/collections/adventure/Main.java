8ae0e4a53f21a9b434644d160b6b324340316d9b
dgarrett <dgarrett@apptricity.com>
2022-03-17 11:39:02 -0500

    Adventure game challenge

--- src/com/javacert/collections/adventure/Main.java
+++ src/com/javacert/collections/adventure/Main.java
@@ -32,6 +32,13 @@ public class Main {
         locations.get(5).addExit("S", 1);
         locations.get(5).addExit("W", 2);
 
+        Map<String, String> vocabulary = new HashMap<>();
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("EAST", "E");
+
 
         int loc = 1;
         while (true){
@@ -47,6 +54,15 @@ public class Main {
             System.out.println();
 
             String direction = scanner.nextLine().toUpperCase();
+            if (direction.length() >1){
+                String[] words = direction.split(" ");
+                for (String word: words){
+                    if (vocabulary.containsKey(word)){
+                        direction = vocabulary.get(word);
+                        break;
+                    }
+                }
+            }
 
             if (exits.containsKey(direction)){
                 loc = exits.get(direction);
