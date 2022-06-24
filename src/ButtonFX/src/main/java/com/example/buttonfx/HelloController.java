87e4358895fb769b9a15b2a16f601782de28660f
dgarrett <dgarrett@apptricity.com>
2022-06-24 16:16:19 -0500

    lamdas

---
+++ src/ButtonFX/src/main/java/com/example/buttonfx/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.buttonfx;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    private Button clickMeButton;
+
+    public void initialize(){
+        clickMeButton.setOnAction(event -> System.out.println("You clicked me!"));
+
+    }
+}
